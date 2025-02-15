a3c6536bc256e9b9edcc9941da0df49cd308347b
benoitAD <benoit.augerdubois@gmail.com>
2025-02-15 12:15:43 +0100

    FIX: changing database config in the config.php file

--- config/config.php
+++ config/config.php
@@ -1,8 +1,21 @@
 <?php
+$db_url = getenv('DATABASE_URL');
+
+if ($db_url) {
+    $db_parts = parse_url($db_url);
     return [
-        'DB_HOST' => getenv('DB_HOST') ?: 'db', // Use "db" to match the service name
+        'DB_HOST' => $db_parts['host'],
+        'DB_NAME' => ltrim($db_parts['path'], '/'),
+        'DB_USER' => $db_parts['user'],
+        'DB_PASS' => $db_parts['pass'],
+    ];
+} else {
+    return [
+        'DB_HOST' => getenv('DB_HOST') ?: 'localhost',
         'DB_NAME' => getenv('DB_NAME') ?: 'ecommerce_demo',
         'DB_USER' => getenv('DB_USER') ?: 'user',
         'DB_PASS' => getenv('DB_PASS') ?: 'password',
     ];
+}
+
 
